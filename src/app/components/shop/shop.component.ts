befbae1d95bb58daa37e1219f4a42393f82577b3
TawaGC <giucalvia99@gmail.com>
2024-09-09 08:17:04 +0200

    .

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoeDataServiceService } from '../../services/shoe-data-service.service';
 import { Filtro, Prodotto } from '../../models/shoeData';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-shop',
@@ -11,39 +9,90 @@ import { debounceTime } from 'rxjs';
 })
 export class ShopComponent implements OnInit{
 
-  filtroForm!: FormGroup;
-  products: Prodotto[] = [];
+  constructor(private sds : ShoeDataServiceService){ }
 
-  constructor(
-    private sds : ShoeDataServiceService,
-    private fb: FormBuilder
-  ){ }
- 
+  products : Prodotto[] = [];
+  categories: String[] = [];
+  colors:String[] = []
+  filters:Filtro = {
+    nome: '',
+    categoria: '',
+    colori_disponibili:[],
+  };
+
+  filteredProducts = [...this.products];
+
   ngOnInit(): void {
-    this.filtroForm = this.fb.group({
-      nome: [''], 
-      categoria: '', 
-      taglie_disponibili: [''], 
-      colori_disponibili: [''],
-      best_seller: '', 
-      nuovo_arrivi: ''
-    });
-
-    this.filtroForm.valueChanges.pipe(debounceTime(300)).subscribe((params) => {
-      this.getFilteredData(params);
-    });
-
-    this.getFilteredData(this.filtroForm.value);
-  };
-
-  getFilteredData(params: Filtro): void {
-    // Chiama il servizio passando i filtri
-    this.sds.getFilteredShoes(params).subscribe(response => {
-      if(Array.isArray(response)){
-        this.products = response;
-        console.log(this.products);
-        console.log(response);
+    this.sds.getFilteredShoes().subscribe(p => {
+      if(Array.isArray(p)){
+        this.products = p;
       };
+
+      this.setCategory();
+      this.setColor();
     });
   };
+
+  applyFilters(){
+    console.log('Filters: ', this.filters);
+    
+    this.filteredProducts = this.products.filter(product => {
+      
+      const matchsName = this.filters.nome ? product.nome.toLowerCase().includes(this.filters.nome.toLowerCase()): true
+            
+      const matchsCategory = this.filters.categoria ? product.categoria.toLowerCase().includes(this.filters.categoria.toLowerCase()): true
+
+      const coloriDisponibili = this.filters.colori_disponibili || [];
+      const prodottiColori = product.colori_disponibili.map(color => color.toLowerCase()); // Converti colori disponibili in minuscolo
+      const matchsColor = coloriDisponibili.length > 0
+      ? coloriDisponibili.some(filterColor =>
+          prodottiColori.includes(filterColor.toLowerCase())
+        ) : true;
+
+        console.log(`Product ${product.nome} ${product.colori_disponibili}: matchesColor=${matchsColor}`);
+
+      return matchsName && matchsCategory && matchsColor;
+    });  
+  };
+
+  setCategory(){
+    const categorySet = new Set<string>();
+    
+    this.products.forEach(product => {
+      categorySet.add(product.categoria);      
+    });
+
+    this.categories = Array.from(categorySet);
+  };
+
+  setColor(){
+    const colorSet = new Set<string>();
+
+    this.products.forEach(product => {
+      
+      product.colori_disponibili.forEach( pColor => {        
+        colorSet.add(pColor);
+      })
+    })
+    
+    this.colors = Array.from(colorSet);
+  };
+
+  onColorChange(event : any){
+    const color = event.target.value;
+    const isChecked = event.target.checked;
+
+    if(isChecked){
+      if (!this.filters.colori_disponibili) {
+        this.filters.colori_disponibili = [];
+      };
+      this.filters.colori_disponibili?.push(color);
+    }else{
+      const index = this.filters.colori_disponibili?.indexOf(color);
+      if(index! > -1){
+        this.filters.colori_disponibili?.splice(index!, 1);
+      };
+    };
+  };
+
 };
