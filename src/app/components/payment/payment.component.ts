4d47688c2dc81e8f6b977f35a566819a2d4e2a4c
TawaGC <giucalvia99@gmail.com>
2024-09-20 15:20:25 +0200

    ,

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CartService } from '../../services/cart-service.service';
+import { CartItem } from '../../models/shoeData';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.scss'
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit{
+
+  cartItems: CartItem[] = [];
 
   userForm: FormGroup;
   cardFrom: FormGroup;
 
   currentStep: number = 1;
 
-  constructor(private fb: FormBuilder){ 
+  constructor(private fb: FormBuilder, public cs: CartService){ 
     
     this.userForm = this.fb.group({
       nome: ['', [Validators.required]],
@@ -29,6 +33,16 @@ export class PaymentComponent {
       scadenza: ["", [Validators.required, Validators.pattern('^(0[1-9]|1[0-2])/[0-9]{2}$')]],
       cvv: ["", [Validators.required, Validators.pattern('^[0-9]{3,4}$')]]
     })
+  }
+
+  ngOnInit(): void {
+    this.cs.getCartItems().subscribe(items => {
+      this.cartItems = items;
+    });
+  }
+
+  getTotal() : number{
+    return this.cartItems.reduce((total, item) => total + (item.prezzo * item.quantita), 0);
   }
 
   nextStep(){
